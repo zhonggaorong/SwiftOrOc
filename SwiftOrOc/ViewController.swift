c8fe6f7581b0be707ea63e3a7636b6e0cd7ace7c
zhonggaorong <458673541@qq.com>
2016-11-15 16:05:25 +0800

    Initial Commit

---
+++ SwiftOrOc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftOrOc
+//
+//  Created by 张国荣 on 16/11/15.
+//  Copyright © 2016年 BateOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
